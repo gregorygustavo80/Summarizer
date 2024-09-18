9d8361837e796e4cdb19084450718a9bf87638bb
gregorygustavo80 <gregorygustavo@hotmail.com>
2024-09-18 11:24:43 -0300

    first commit

---
+++ streamlit.py
@@ -0,0 +1,80 @@
+import streamlit as st
+from pytubefix import YouTube
+import ffmpeg
+from transformers import pipeline, BartTokenizer, BartForConditionalGeneration
+
+st.title("Resuma vídeos do YouTube")
+
+video_url = st.text_input("Cole o link do YouTube aqui:")
+
+def baixar_audio(video_url, progress_bar):
+    try:
+        yt = YouTube(video_url)
+        audio_stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
+        audio_stream.download(filename='input_audio.mp3')
+        progress_bar.progress(33) 
+        st.success("Áudio baixado com sucesso!")
+    except Exception as e:
+        st.error(f"Erro ao baixar o áudio: {e}")
+
+def converter_audio(progress_bar):
+    try:
+        input_audio = 'input_audio.mp3'
+        output_audio = 'output_audio.wav'
+        ffmpeg.input(input_audio).output(output_audio).run()
+        progress_bar.progress(66)  
+        st.success("Áudio convertido com sucesso!")
+    except Exception as e:
+        st.error(f"Erro ao converter o áudio: {e}")
+
+def transcrever_audio(progress_bar):
+    try:
+        asr_model = pipeline("automatic-speech-recognition", model="openai/whisper-tiny")
+        transcription = asr_model('output_audio.wav')
+        texto = transcription['text']
+        progress_bar.progress(90)  
+        st.success("Transcrição concluída com sucesso!")
+        return texto
+    except Exception as e:
+        st.error(f"Erro ao transcrever o áudio: {e}")
+        return ""
+
+def resumir(texto, progress_bar):
+    try:
+        model_name = "facebook/bart-base"
+        tokenizer = BartTokenizer.from_pretrained(model_name)
+        model = BartForConditionalGeneration.from_pretrained(model_name)
+
+        inputs = tokenizer(texto, return_tensors="pt", max_length=1024, truncation=True)
+        summary_ids = model.generate(inputs["input_ids"], max_length=800, min_length=400, length_penalty=2.0, num_beams=4, early_stopping=True)
+        summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
+        
+        progress_bar.progress(100) 
+        st.success("Resumo gerado com sucesso!")
+        return summary
+    except Exception as e:
+        st.error(f"Erro ao gerar o resumo: {e}")
+        return ""
+
+# Botões para iniciar cada processo
+if st.button("Resumir vídeo"):
+    if video_url:
+        # Subtítulo para a etapa de processamento
+        st.subheader("Processando...")
+
+        # Criando a barra de progresso
+        progress_bar = st.progress(0)
+
+        baixar_audio(video_url, progress_bar)
+        converter_audio(progress_bar)
+        texto = transcrever_audio(progress_bar)
+
+        if texto:
+            # Subtítulo para o resumo
+            st.subheader("Resumo do Áudio")
+            resumo = resumir(texto, progress_bar)
+            st.write(resumo)
+        else:
+            st.warning("Não foi possível transcrever o áudio, portanto o resumo não foi gerado.")
+    else:
+        st.warning("Por favor, insira um link válido do YouTube.")
---
+++ summarizer.py
@@ -0,0 +1,63 @@
+from pytubefix import YouTube
+import ffmpeg
+from transformers import pipeline, BartTokenizer, BartForConditionalGeneration
+
+video_url = input('Cole o link do YouTube aqui: ')
+
+def baixar_audio():
+    try:
+        yt = YouTube(video_url)
+        audio_stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
+        audio_stream.download(filename='input_audio.mp3')
+        print("Áudio baixado com sucesso!")
+    except Exception as e:
+        print(f"Erro ao baixar o áudio: {e}")
+
+def converter_audio():
+    try:
+        input_audio = 'input_audio.mp3'
+        output_audio = 'output_audio.wav'
+        ffmpeg.input(input_audio).output(output_audio, ar=16000).run()
+        print("Áudio convertido com sucesso!")
+    except Exception as e:
+        print(f"Erro ao converter o áudio: {e}")
+
+def transcrever_audio():
+    try:
+        asr_model = pipeline("automatic-speech-recognition", model="openai/whisper-tiny")
+        transcription = asr_model('output_audio.wav')
+        texto = transcription['text']
+        print("Transcrição concluída com sucesso!")
+        return texto
+    except Exception as e:
+        print(f"Erro ao transcrever o áudio: {e}")
+        return ""
+
+def resumir(texto):
+    try:
+        model_name = "facebook/bart-base"
+        tokenizer = BartTokenizer.from_pretrained(model_name)
+        model = BartForConditionalGeneration.from_pretrained(model_name)
+
+        inputs = tokenizer(texto, return_tensors="pt", max_length=1024, truncation=True)
+        summary_ids = model.generate(inputs["input_ids"], max_length=800, min_length=400, length_penalty=2.0, num_beams=4, early_stopping=True)
+        summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
+        print("Resumo gerado com sucesso!")
+        return summary
+    except Exception as e:
+        print(f"Erro ao gerar o resumo: {e}")
+        return ""
+
+def main():
+    baixar_audio()
+    converter_audio()
+    texto = transcrever_audio()
+    if texto:
+        resumo = resumir(texto)
+        print(f'Resumo: {resumo}')
+        
+    else:
+        print("Não foi possível transcrever o áudio, portanto o resumo não foi gerado.")
+
+if __name__ == "__main__":
+    main()
