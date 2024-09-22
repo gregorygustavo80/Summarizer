8e8b2e0917977a287e584591bd29ef1a638e7ed7
gregorygustavo80 <gregorygustavo@hotmail.com>
2024-09-22 07:43:38 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -56,7 +56,7 @@ def resumir(texto, progress_bar):
         model = BartForConditionalGeneration.from_pretrained(model_name)
 
         inputs = tokenizer(texto, return_tensors="pt", max_length=1024, truncation=True)
-        summary_ids = model.generate(inputs["input_ids"], max_length=800, min_length=400, length_penalty=2.0, num_beams=8, early_stopping=True)
+        summary_ids = model.generate(inputs["input_ids"], max_length=800, min_length=400, length_penalty=2.0, num_beams=4, early_stopping=True)
         summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
         
         progress_bar.progress(100) 
